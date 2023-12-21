4e1999232a11c6f0b460b387cf5c424427aa58c6
LazeIsCoding <116916398+LazeIsCoding@users.noreply.github.com>
2023-12-21 19:54:44 +0100

    Added Camera follow

---
+++ Entities/Player.go
@@ -0,0 +1,39 @@
+package Entities
+
+import (
+	"fmt"
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+type Player struct {
+	Y      int32
+	X      int32
+	Health int32
+	Speed  int32
+	Width  float32
+	Height float32
+	Sprite rl.Texture2D
+}
+
+func NewPlayer(x, y, health, speed int32, spritePath string) *Player {
+	sprite := rl.LoadTexture(spritePath)
+	return &Player{
+		X:      x,
+		Y:      y,
+		Health: health,
+		Speed:  speed,
+		Sprite: sprite,
+		Width:  float32(sprite.Width),
+		Height: float32(sprite.Height),
+	}
+}
+
+func (p *Player) Move(x, y int32) {
+	p.X += x
+	p.Y += y
+}
+
+func (p *Player) ToString() string {
+	s := fmt.Sprint("X: ", p.X, "Y: ", p.Y)
+	return s
+}
--- game/game.go
+++ game/game.go
@@ -1,48 +1,108 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	"fmt"
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+import ent "GoGoGo/Entities"
 
-type Entity struct {
-	Y      int32
-	X      int32
-	sprite rl.Texture2D
-}
+const (
+	screenWidth  = 512
+	screenHeight = 374
+)
 
-var Character Entity
+var (
+	running   = true
+	Character *ent.Player
+
+	playerUp, playerDown, playerRight, playerLeft bool
+	playerMoving                                  bool
+
+	cam rl.Camera2D
+)
 
 func initialize() {
-	rl.InitWindow(528, 528, "CozyTown")
-	Character.X = 100
-	Character.Y = 100
-	Character.sprite = rl.LoadTexture("Assets/PlayerFemale.png")
-}
-
-func update() {
-
+	rl.SetExitKey(0)
+	rl.SetTargetFPS(60)
+	rl.InitWindow(screenWidth, screenHeight, "CozyTown")
+	Character = ent.NewPlayer(100, 100, 100, 1, "Assets/PlayerFemale.png")
+	cam = rl.NewCamera2D(rl.NewVector2(screenWidth/2, screenHeight/2), rl.NewVector2(float32(Character.X)+Character.Width/2, float32(Character.Y)+Character.Height/2), 0, 3)
+	fmt.Print(Character.Width, " H:", Character.Height)
 }
 
 func input() {
-	if rl.IsKeyPressed(rl.KeyD) {
-		Character.X++
+	if rl.IsKeyDown(rl.KeyW) || rl.IsKeyDown(rl.KeyUp) {
+		playerMoving = true
+		playerUp = true
+	}
+	if rl.IsKeyDown(rl.KeyS) || rl.IsKeyDown(rl.KeyDown) {
+		playerMoving = true
+		playerDown = true
+	}
+	if rl.IsKeyDown(rl.KeyA) || rl.IsKeyDown(rl.KeyLeft) {
+		playerMoving = true
+		playerLeft = true
+	}
+	if rl.IsKeyDown(rl.KeyD) || rl.IsKeyDown(rl.KeyRight) {
+		playerMoving = true
+		playerRight = true
+	}
+	if rl.IsKeyPressed(rl.KeyF) {
+		rl.ToggleFullscreen()
 	}
 }
-func render() {
-	rl.DrawTexture(Character.sprite, Character.X, Character.Y, rl.White)
+
+func update() {
+	running = !rl.WindowShouldClose()
+	if playerMoving {
+		if playerUp {
+			Character.Move(0, -Character.Speed)
+		}
+		if playerDown {
+			Character.Move(0, Character.Speed)
+		}
+		if playerLeft {
+			Character.Move(-Character.Speed, 0)
+		}
+		if playerRight {
+			Character.Move(Character.Speed, 0)
+		}
+	}
+
+	playerMoving = false
+	playerUp, playerDown, playerRight, playerLeft = false, false, false, false
+
+	cam.Target = rl.NewVector2(float32(Character.X)+Character.Width/2, float32(Character.Y)+Character.Height/2)
 }
 
+func render() {
+	rl.BeginDrawing()
+	rl.ClearBackground(rl.White)
+	rl.BeginMode2D(cam)
+	drawScene()
+	rl.EndMode2D()
+	rl.EndDrawing()
+}
+
+func quit() {
+	rl.UnloadTexture(Character.Sprite)
+	rl.CloseWindow()
+}
+
+func drawScene() {
+
+	rl.DrawTexture(Character.Sprite, Character.X, Character.Y, rl.White)
+
+}
 func main() {
 
 	initialize()
 
-	for !rl.WindowShouldClose() {
-		rl.BeginDrawing()
-		
+	for running {
 		input()
 		update()
 		render()
-
-		rl.EndDrawing()
 	}
-	//quit()
-	rl.CloseWindow()
+	quit()
+
 }
